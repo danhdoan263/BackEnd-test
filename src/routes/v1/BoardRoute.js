dccdb67731fe31250c6fea979a2a5462e4ab9805
danhdoan263 <doan.working@gmail.com>
2024-10-09 21:15:03 +0700

    init boardRoute
    setting route v1 => board

--- src/routes/v1/BoardRoute.js
+++ src/routes/v1/BoardRoute.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express';
+import { StatusCodes } from "http-status-codes";
+
+const Router = express.Router()
+
+Router.route('/')
+    .get((req, res) => {
+        res.status(StatusCodes.ACCEPTED).json({
+            message: 'route / accepted',
+            code: StatusCodes.ACCEPTED
+        })
+    })
+    .post()
+
+export const boardRouter = Router
---
+++ src/routes/v1/index.js
@@ -0,0 +1,15 @@
+// quản lí router
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRouter } from "~/routes/v1/BoardRoute";
+
+
+const Router = express.Router()
+Router.use('/board', boardRouter)
+
+Router.get('/', (req, res) => {
+    res.end('<h1>API v1 ready to use!</h1>')
+})
+
+
+export const API_v1 = Router
