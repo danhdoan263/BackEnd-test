2e755d9c8c3abc5f09bf6134000f32976d712d84
danhdoan263 <doan.working@gmail.com>
2024-11-07 01:26:18 +0700

    comment function

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,110 +3,170 @@ import { postService } from '~/services/postService';
 import { userService } from '~/services/userService';
 
 const getAllPost = async (req, res, next) => {
-  const response = await postService.getAllPost();
+  try {
+    const posts = await postService.getAllPost();
+    if (!posts.length) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'No posts found', data: [] });
+    }
 
-  //get user id without duplicate by ...new Set
-  const userids = [...new Set(response.map((response) => response.user_id))];
+    const userIds = [...new Set(posts.map((post) => post.user_id))];
+    const users = await userService.getUserById(userIds);
 
-  // after get post get info post's user by list id
-  const postuser = await userService.getUserById(userids);
+    const userInfo = users.reduce((acc, user) => {
+      acc[user._id.toString()] = {
+        full_name: user.full_name,
+        profile_url_img: user.profile_url_img,
+      };
+      return acc;
+    }, {});
 
-  const nameAndIds = postuser.reduce((acc, user) => {
-    acc[user._id.toString()] = {
-      full_name: user.full_name,
-      profile_url_img: user.profile_url_img,
-    };
-    return acc;
-  }, {});
-
-  //insert full name to post by id
-  const data = response.map((post) => {
-    const inserName = nameAndIds[post.user_id] || 'unknow user';
-    return {
+    const data = posts.map((post) => ({
       ...post,
-      ...inserName,
-    };
-  });
+      ...(userInfo[post.user_id] || { full_name: 'unknown user' }),
+    }));
 
-  //If there is a response from the service (data is not empty)
-  if (response && response.length > 0) {
-    return res.status(StatusCodes.OK).json({
-      message: 'Query successful',
-      data: data,
-    });
+    res.status(StatusCodes.OK).json({ message: 'Query successful', data });
+  } catch (error) {
+    next(error);
   }
-
-  // Empty response case
-  return res.status(StatusCodes.NOT_FOUND).json({
-    message: 'No posts found',
-    data: [],
-  });
 };
 
-//in like post get user_id from auth token and get post_id from params
 const likePost = async (req, res, next) => {
   const { post_id } = req.query;
-  console.log(req.query);
-
   if (!post_id) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      message: 'Post ID is required',
-    });
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'Post ID is required' });
   }
 
   const userLike_id = req.user;
 
-  //update like to userList
-  const response = await postService.likePost(post_id, userLike_id);
-  console.log(response);
-  const countLike = await postService.countLike(post_id);
-  if (response) {
-    res.status(StatusCodes.OK).json({
-      message: 'like post successfully',
-      info: response,
-      likes: countLike.userList.length,
-    });
-  } else {
-    res.status(StatusCodes.NOT_FOUND).json({
-      message: 'Post not found or like action failed',
-    });
+  try {
+    const response = await postService.likePost(post_id, userLike_id);
+    const countLike = await postService.countLike(post_id);
+
+    response
+      ? res.status(StatusCodes.OK).json({
+          message: 'like post successfully',
+          info: response,
+          likes: countLike.userList.length,
+        })
+      : res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: 'Post not found or like action failed' });
+  } catch (error) {
+    next(error);
   }
 };
+
 const getLikePost = async (req, res) => {
-  //count likes in post = user_id in userList
   const { post_id } = req.query;
-  const countLike = await postService.countLike(post_id);
-  if (countLike) {
+
+  try {
+    const countLike = await postService.countLike(post_id);
     res.status(StatusCodes.OK).json({
       message: 'like post successfully',
       likes: countLike.userList.length,
     });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error fetching like count' });
   }
 };
 
 const deletePost = async (req, res) => {
   const { post_id } = req.query;
 
-  //run 2 await need dive 2 threads
-  const [response, responseScore] = await Promise.all([
-    postService.deletePost(post_id),
-    postService.deletePostScore(post_id),
-  ]);
+  try {
+    const [response, responseScore] = await Promise.all([
+      postService.deletePost(post_id),
+      postService.deletePostScore(post_id),
+    ]);
 
-  if (response.deletedCount !== 0) {
-    res.status(StatusCodes.OK).json({
-      message: `deleted post id:${post_id}`,
-      code: StatusCodes.OK,
-      response: response,
-      postScore: responseScore,
-    });
-  } else {
-    res.status(StatusCodes.NOT_FOUND).json({
-      message: `post id: ${post_id} not found`,
-      code: StatusCodes.NOT_FOUND,
-      response: response,
-      postScore: responseScore,
-    });
+    response.deletedCount !== 0
+      ? res.status(StatusCodes.OK).json({
+          message: `deleted post id:${post_id}`,
+          code: StatusCodes.OK,
+          response,
+          postScore: responseScore,
+        })
+      : res.status(StatusCodes.NOT_FOUND).json({
+          message: `post id: ${post_id} not found`,
+          code: StatusCodes.NOT_FOUND,
+          response,
+          postScore: responseScore,
+        });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error deleting post' });
+  }
+};
+
+const creteNewComment = async (req, res) => {
+  // const [data] = req.body.commentData;
+  // if (data.parent_id) {
+  //   try {
+  //     const response = await postService.createNewReply(data);
+  //     res.status(StatusCodes.OK).json({ data, response });
+  //   } catch (error) {
+  //      res
+  //        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+  //        .json({ message: 'Error creating comment' });
+  //   }
+  // }
+  // else {
+  //   try {
+  //     const response = await postService.creteNewComment(data);
+  //     res.status(StatusCodes.OK).json({ data, response });
+  //   } catch (error) {
+  //     res
+  //       .status(StatusCodes.INTERNAL_SERVER_ERROR)
+  //       .json({ message: 'Error creating comment' });
+  //   }
+  // }
+
+  const [data] = req.body.commentData;
+  const createCommentFn = data.parent_id
+    ? postService.createNewReply
+    : postService.creteNewComment;
+
+  try {
+    const response = await createCommentFn(data);
+    res.status(StatusCodes.OK).json({ data, response });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error creating comment' });
+  }
+};
+
+const getAllComment = async (req, res) => {
+  try {
+    const response = await postService.getAllComment();
+    response
+      ? res.status(StatusCodes.OK).json({ comment: response })
+      : res.status(StatusCodes.NOT_FOUND).json({ comment: 'not found' });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error fetching comments' });
+  }
+};
+
+const findCommentByPostId = async (req, res) => {
+  const { post_id } = req.query;
+
+  try {
+    const response = await postService.findCommentByPostId(post_id);
+    res.status(StatusCodes.OK).json({ comment: response });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error fetching comments by post ID' });
   }
 };
 
@@ -115,4 +175,7 @@ export const postController = {
   likePost,
   getLikePost,
   deletePost,
+  creteNewComment,
+  getAllComment,
+  findCommentByPostId,
 };
